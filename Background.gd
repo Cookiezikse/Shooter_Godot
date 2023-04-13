99cfb36871c2493025f6448138f2f24111caa9ff
Cookie <yumzuko@gmail.com>
2023-04-13 19:59:31 +0200

    Changements du script du background
    La map se remet comme pour l'eau, ce qui permet un niveau infini

--- Background.gd
+++ Background.gd
@@ -2,9 +2,11 @@ extends ParallaxBackground
 
 @export var speed : int
 @onready var background = $WaterBackground
+@onready var background2 = $MapBackground
 
 #Background qui marche, on peut changer la vitesse du scroll avec la variable export speed
 func _process(delta):
 	scroll_base_offset -= Vector2(0,-speed) * delta
 func _ready():
-	background.motion_mirroring.y = 100
+	background.motion_mirroring.y = 100 #Ne pas changer cette valeur (normalement)
+	background2.motion_mirroring.y = 1200 #Changer cette valeur si on agrandi la map derriere
