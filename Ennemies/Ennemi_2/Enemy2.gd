17306509916293e597b2bb48fae67d110bdbb39e
Cookie <yumzuko@gmail.com>
2023-04-14 18:43:22 +0200

    Ennemi 2 changements + game_over

--- Ennemies/Ennemi_2/Enemy2.gd
+++ Ennemies/Ennemi_2/Enemy2.gd
@@ -1,25 +1,23 @@
 extends CharacterBody2D
 
 var Bullet = preload("res://Ennemies/enemy_bullet.tscn")
-
+var Player_pos #Faire une variable qui a les coordonnées du player
 var player = null
 var canshoot = true
 
-@export var speed = 2
+@export var speed = 5
 @export var health = 2
 
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("Player"):
-		player = true
+		player = body
 		
 func _on_area_2d_body_exited(body):
 	player = null
-
 		
 func _physics_process(delta):
 	velocity.y = speed
-	velocity.x = 0
-	if player:
+	if player && velocity > 0:
 		velocity = position.direction_to(player.position) * speed
 	move_and_collide(velocity)
 
--- Global.gd
+++ Global.gd
@@ -1,3 +1,5 @@
 extends Node
 
 var score = 0
+
+#Faire une variable pour l'amélioration du joueur pour que dans le shop on la sélec
