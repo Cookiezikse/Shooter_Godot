7d457ec3651dec43698fa8216cec9bc8add37fa6
ShankyFR <115532997+ShankyFR@users.noreply.github.com>
2023-04-14 11:36:38 +0200

    Enemy2 + detection box enemy 1 changement

---
+++ Enemy2.gd
@@ -0,0 +1,32 @@
+extends CharacterBody2D
+
+var Bullet = preload("res://enemy_bullet.tscn")
+
+var player = null
+var canshoot = true
+
+@export var speed = 3
+@export var health = 2
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("Player"):
+		player = true
+		
+func _on_area_2d_body_exited(body):
+	player = null
+
+		
+func _physics_process(delta):
+	velocity.y = speed
+	velocity.x = 0
+	if player:
+		velocity = position.direction_to(player.position)
+		velocity.y = speed
+	move_and_collide(velocity)
+
+		
+func enemy_hit( ):
+	health -= 1
+	if health == 0:
+		queue_free()
+
