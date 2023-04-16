bd82ac8785971e493126eaa10728a293acfd919e
Cookie <yumzuko@gmail.com>
2023-04-16 14:07:23 +0200

    changement

--- Menus/Game Over/gameovermenu.gd
+++ Menus/Game Over/gameovermenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_restart_body_entered(body):
-	get_tree( ).change_scene_to_file(Global.Scene_story)
+	get_tree( ).change_scene_to_file(Global.Scene2)
 
 func _on_quit_body_entered(body):
 	get_tree().change_scene_to_file("res://Menus/Main Menu/Menu.tscn")
