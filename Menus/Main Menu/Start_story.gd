214b7dcf2fb7dc95f1ab3fd7e9315123a2d1cd55
Cookie <yumzuko@gmail.com>
2023-04-17 20:36:52 +0200

    COWWWWWWWWW MAP NIVEAU

--- Menus/Main Menu/Start_story.gd
+++ Menus/Main Menu/Start_story.gd
@@ -5,7 +5,8 @@ var player = false
 func _process(delta):
 	if player:
 		if Input.is_action_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://Menus/Credits/Credits.tscn")
+			Global.Scene2 = "res://Menus/Select Menu/select_menu1.tscn"
+			get_tree().change_scene_to_file("res://Menus/Main Menu/menu_difficulty.tscn")
 
 func _on_body_entered(body):
 	player = true
