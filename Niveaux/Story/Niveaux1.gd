a0d0aa17069835b3e1bd1d125fdad2cc005e7899
Cookie <yumzuko@gmail.com>
2023-04-17 19:30:39 +0200

    Changement dans le niveau 1

--- Niveaux/Story/Niveaux1.gd
+++ Niveaux/Story/Niveaux1.gd
@@ -3,15 +3,10 @@ extends Node2D
 var Enemy = preload("res://Ennemies/Ennemi_1/enemy.tscn")
 var Enemy2 = preload("res://Ennemies/Ennemi_2/Enemy2.tscn")
 var random = 0
+const count = 20
 func _on_spawn_timeout():
-	if Global.Death_ennemi1 > 12:
+	if count < Global.Death_ennemi1:
 		get_tree().change_scene_to_file("res://Menus/Select Menu/select_menu1.tscn")
-#	random = (randi() %2 + 1)
-#	if random == 1:
-#		var enemy = Enemy.instantiate()
-#		add_child(enemy)
-#		enemy.position = Vector2(randi_range(2,580),randi_range(-5,-5))
-#	if random == 2: 
-#		var enemy = Enemy2.instantiate()
-#		add_child(enemy)
-#		enemy.position = Vector2(randi_range(2,580),randi_range(-5,-5))
+	var enemy = Enemy.instantiate()
+	add_child(enemy)
+	enemy.position = Vector2(randi_range(2,580),randi_range(-5,-5))
