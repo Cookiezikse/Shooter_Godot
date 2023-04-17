c856158f47c1610c77dd4b01a0ffe185d365759b
Cookie <yumzuko@gmail.com>
2023-04-17 13:34:00 +0200

    uihergiog

--- Menus/Main Menu/Start_story.gd
+++ Menus/Main Menu/Start_story.gd
@@ -5,9 +5,7 @@ var player = false
 func _process(delta):
 	if player:
 		if Input.is_action_pressed("ui_accept"):
-			emit_signal("JOUEURS")
-			Global.Scene2 = "res://Menus/Select Menu/select_menu1.tscn"
-			get_tree().change_scene_to_file("res://Menus/Main Menu/menu_difficulty.tscn")
+			get_tree().change_scene_to_file("res://Menus/Credits/Credits.tscn")
 
 func _on_body_entered(body):
 	player = true
