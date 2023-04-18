27d6a027a32922a7309ab7e390a4e6a997892c30
Cookie <yumzuko@gmail.com>
2023-04-18 10:00:48 +0200

    psugh

--- Menus/Main Menu/Hard.gd
+++ Menus/Main Menu/Hard.gd
@@ -5,8 +5,8 @@ var player = false
 func _process(delta):
 	if player:
 		if Input.is_action_pressed("ui_accept"):
-			EnnemiVars.Speed_Ennemi_1 = 0.7
-			EnnemiVars.Health_Ennemi_1 = 4
+			EnnemiVars.Speed_Ennemi_1 = 0.8
+			EnnemiVars.Health_Ennemi_1 = 3
 			EnnemiVars.Spawn_point_Boss_1 = 15
 			EnnemiVars.Speed_atk_Boss_1 = 0.1
 			EnnemiVars.Health_Boss_1 = 100
--- Menus/Main Menu/Medium.gd
+++ Menus/Main Menu/Medium.gd
@@ -5,7 +5,7 @@ var player = false
 func _process(delta):
 	if player:
 		if Input.is_action_pressed("ui_accept"):
-			EnnemiVars.Speed_Ennemi_1 = 1
+			EnnemiVars.Speed_Ennemi_1 = 1.2
 			EnnemiVars.Health_Ennemi_1 = 3
 			EnnemiVars.Spawn_point_Boss_1 = 11
 			EnnemiVars.Speed_atk_Boss_1 = 0.15
