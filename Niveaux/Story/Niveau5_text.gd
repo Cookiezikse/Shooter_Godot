c5d78d8045c0ab86360a9c4b0cf8d4ef15034089
Cookie <yumzuko@gmail.com>
2023-04-19 20:54:50 +0200

    Text niveau 5

--- Niveaux/Story/Niveau5_text.gd
+++ Niveaux/Story/Niveau5_text.gd
@@ -9,5 +9,12 @@ func _on_animation_player_animation_finished(anim_name):
 		AnimationText.play("Text2")
 		anim_temp = 1 
 	if anim == 1:
-		pass
+		AnimationText.play("Text3")
+		anim_temp = 2
+	if anim == 2:
+		AnimationText.play("Text4")
+		anim_temp = 3
+	if anim == 3:
+		AnimationText.play("Text5")
+		anim_temp = 4
 	anim = anim_temp
