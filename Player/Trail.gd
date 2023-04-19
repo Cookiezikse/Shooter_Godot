7f34253b386447dcf0a51c459b7be308779e2ffe
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2023-04-19 11:50:58 +0200

    trail2

--- Player/Trail.gd
+++ Player/Trail.gd
@@ -1,13 +1,21 @@
 extends Line2D
 
+var target
 var point
+#@export var targetPath
+@export var trailLength = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	target = get_node(targetPath)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	point=global_position
-	add_point(point)
+#	global_position = Vector2(0,0)
+#	global_rotation = 0
+#	point = target.global_position
+#	add_point(point)
+#	while get_point_count() > trailLength:
+#		remove_point(0)
 	pass
