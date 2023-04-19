9bc654f6ec54677324ad502e6dccf4a55d32235b
Cookie <yumzuko@gmail.com>
2023-04-19 10:25:29 +0200

    Boss 3

--- Ennemies/Boss2/bullet_boss2.gd
+++ Ennemies/Boss2/bullet_boss2.gd
@@ -10,22 +10,32 @@ var count = 0
 var count2 = 0
 var Vector = Vector2(0,velocity.x) 
 var random = (randi_range(1,2))
+var random2 = (randi_range(1,2))
 var CHOISIR = 1
 
 func _process(delta):
 	#position.x += (-9.81*(delta*delta))/2 + speed * sin(teta)
 	#position.y += 9.81 * delta
 	if CHOISIR == 1:
-		if random == 1:
-			velocity.y = speed - 150
-			position.x = sin((position.y)*0.02)*400
-		if random == 2:
-			velocity.y = speed - 200
-			position.x = sin((position.y)*0.02)*400 + 600
+		if random2 == 1:
+			if random == 1:
+				velocity.y = speed - 150
+				position.x = sin((position.y)*0.02)*400
+			if random == 2:
+				velocity.y = speed - 200
+				position.x = sin((position.y)*0.02)*400 + 600
+		if random2 == 2:
+			if random == 1:
+				velocity.y = speed - 150
+				position.x = cos((position.y)*0.02)*400
+			if random == 2:
+				velocity.y = speed - 200
+				position.x = cos((position.y)*0.02)*400 + 600
+			
 		velocity.y = speed - 200
 	if CHOISIR == 2:
 		velocity.y = speed 
-		position.x += sin(position.y/100)*5
+		position.x += sin(position.y/150)*5
 	
 	move_and_slide()
 	
