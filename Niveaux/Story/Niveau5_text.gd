393f23a6fba90e391fb132dcfb52d204292d19cc
Cookie <yumzuko@gmail.com>
2023-04-19 20:37:52 +0200

    Script animations avant boss niveau 5

--- Niveaux/Story/Niveau5_text.gd
+++ Niveaux/Story/Niveau5_text.gd
@@ -1,8 +1,13 @@
 extends Control
 
 @onready var AnimationText = $AnimationPlayer
-
-
+var anim = 0
+var anim_temp = 0
 
 func _on_animation_player_animation_finished(anim_name):
-	AnimationText.play("Text2")
+	if anim == 0:
+		AnimationText.play("Text2")
+		anim_temp = 1 
+	if anim == 1:
+		pass
+	anim = anim_temp
