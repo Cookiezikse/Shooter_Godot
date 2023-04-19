be38497b33f081327e4493c09cd6053bf6776c2e
Mguellaff <115532700+Mguellaff@users.noreply.github.com>
2023-04-19 11:16:16 +0200

    trail

---
+++ Player/Trail.gd
@@ -0,0 +1,13 @@
+extends Line2D
+
+var point
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	point=global_position
+	add_point(point)
+	pass
