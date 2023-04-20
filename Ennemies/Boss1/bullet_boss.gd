ef46c6fbaa2f7933a02d8fc5d5315c1fc6b2b031
Cookie <yumzuko@gmail.com>
2023-04-20 11:56:29 +0200

    Fenetre de jeu réglé et les bullets

--- Ennemies/Boss1/bullet_boss.gd
+++ Ennemies/Boss1/bullet_boss.gd
@@ -6,7 +6,7 @@ const speed = 200
 
 func _process(delta):
 	position += transform.x * speed * delta
-	if Global.Game_Over == 1:
+	if Global.Game_Over == true:
 		Collision.disabled = true
 	if Global.grenade1 >= 0 && Input.is_action_pressed("grenade1"): 
 		queue_free()
