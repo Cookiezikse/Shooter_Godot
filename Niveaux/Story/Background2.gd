70a9575408c596791bb1924d3eefbef2cedf3160
Cookie <yumzuko@gmail.com>
2023-04-20 07:51:51 +0200

    Changement background

---
+++ Niveaux/Story/Background2.gd
@@ -0,0 +1,12 @@
+extends ParallaxBackground
+
+@export var speed : int
+@onready var background1 = $WaterBackground
+@onready var background2 = $MapBackground
+
+#Background qui marche, on peut changer la vitesse du scroll avec la variable export speed
+func _process(delta):
+	scroll_base_offset -= Vector2(0,-speed) * delta
+func _ready():
+	background1.motion_mirroring.y = 100 #Ne pas changer cette valeur (normalement)
+	background2.motion_mirroring.y = 1730 #Changer cette valeur si on agrandi la map derriere
